558045732b4e4cc5e38255eb42d2d51ca96368ed
Miguel Ramirez <31163639+Mike0600@users.noreply.github.com>
2022-03-09 20:21:30 -0600

    Configuration_for_prod_and_dev

--- pregunta2/settings/base.py
+++ pregunta2/settings/base.py
@@ -13,19 +13,21 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-trmhq@qknu2k3a9ngm^k-j3+@2^&f&0v)gzsa37l=+1f&-txbm'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
 
 
 # Application definition
---
+++ pregunta2/settings/dev.py
@@ -0,0 +1,9 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-trmhq@qknu2k3a9ngm^k-j3+@2^&f&0v)gzsa37l=+1f&-txbm'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
